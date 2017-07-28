06cb0be15bb872408fe544648b951434409f28cf
Shontae Hogan <shontaehogan@gmail.com>
2017-07-28 01:36:32 -0400

    completed teh plan and steps for javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+// select and store items and elements working with
+
+// "submit" event for getting search term
+
+// fetch request to be called after a submission
+
+// append fetch results to the page
+
+// click event that will play the song in the audio player
