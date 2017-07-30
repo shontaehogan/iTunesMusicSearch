1f3952f9ce8640d53cd253236b3045c674f6a195
Shontae Hogan <shontaehogan@gmail.com>
2017-07-30 18:20:03 -0400

    debugged and made changes to js code

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ let url = "https://itunes.apple.com/search?term="
 // "submit" event for getting search term
 search.addEventListener('keyup', function(e) {
 
-// fetch request to be called after a submission
-let term = search.value
-fetch(url + term)
-.then(function(response) {
-  if (response.status !== 200) {
-    console.log(response.status)
-    return
-  }
-// append fetch results to the page
-response.json().then(function(data) {
-  let htmlstr = ``data.results.forEach(function(data) {
-    htmlstr += `
+      // fetch request to be called after a submission
+      let term = search.value
+      fetch(url + term)
+        .then(function(response) {
+            if (response.status !== 200) {
+              console.log(response.status)
+              return
+            }
+            // append fetch results to the page
+            response.json().then(function(data) {
+                  let htmlstr = ``
+                  data.results.forEach(function(data) {
+                    htmlstr += `
       <div class ="searchresult">
         <a href = "${data.previewUrl}">
           <img class="image" src="${data.artworkUrl100}"><br>
@@ -26,16 +27,18 @@ response.json().then(function(data) {
           <p class="name">${data.artistName}</p>
       </div> `
 
-    })
+                  })
 
-// click event that will play the song in the audio player
-main.innerHTML = htmlstr
-let albums = document.querySelectorAll(".searchresult a")
+                  // click event that will play the song in the audio player
+                  main.innerHTML = htmlstr
+                  let albums = document.querySelectorAll(".searchresult a")
 
-albums.forEach(function(listen) {
-  listen.addEventListener("click", function(event) {
-    event.preventDefault();
-    let url = listen.getAttribute("href");
-    document.querySelector("audio").setAttribute("src", url);
-  })
+                  albums.forEach(function(listen) {
+                    listen.addEventListener("click", function(event) {
+                      event.preventDefault();
+                      let url = listen.getAttribute("href");
+                      document.querySelector("audio").setAttribute("src", url);
+                    })
+                  })
+})
 })
