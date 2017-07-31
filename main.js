61165e0eca567f646d6fb789b3a2dece726c7d5f
Shontae Hogan <shontaehogan@gmail.com>
2017-07-30 20:43:06 -0400

    changes to css and debugged js so that music plays

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
-// select and store items and elements working with
-let main = document.querySelector('.resultlist')
-let search = document.querySelector('.searchBar')
+
+/*
+  Here is a rough idea for the steps you could take:
+*/
+
+// 1. First select and store the elements you'll be working with
+let main = document.querySelector(".resultlist")
+let search = document.querySelector(".searchBar")
 let url = "https://itunes.apple.com/search?term="
+// 2. Create your `submit` event for getting the user's search term
+search.addEventListener('keyup', function(e){
+// 3. Create your `fetch` request that is called after a submission
+	let term = search.value
+	fetch(url+term)
+		.then(
+	      function(response) {
+	        if (response.status !== 200) {
+	          console.log(response.status)
+	          return
+	        }
+// 4. Create a way to append the fetch results to your page
+			response.json().then(function(data) {
+				let htmlstr = ``
+				data.results.forEach(function(data){
+					htmlstr += `
+					<div class="searchresult">
+							<a href="${data.previewUrl}">
+							<img class="image" src="${data.artworkUrl100}"><br>
+							</a>
+					        <p class="title">${data.trackName}</p><br>
+        					<p class="name">${data.artistName}</p>
+     				 </div>
+					`
+				})
+// 5. Create a way to listen for a click that will play the song in the audio play
+				main.innerHTML = htmlstr
+					let albums = document.querySelectorAll('.searchresult a')
 
-// "submit" event for getting search term
-search.addEventListener('keyup', function(e) {
-
-      // fetch request to be called after a submission
-      let term = search.value
-      fetch(url + term)
-        .then(function(response) {
-            if (response.status !== 200) {
-              console.log(response.status)
-              return
-            }
-            // append fetch results to the page
-            response.json().then(function(data) {
-                  let htmlstr = ``
-                  data.results.forEach(function(data) {
-                    htmlstr += `
-      <div class ="searchresult">
-        <a href = "${data.previewUrl}">
-          <img class="image" src="${data.artworkUrl100}"><br>
-        </a>
-          <p class="title">${data.trackName}</p><br>
-          <p class="name">${data.artistName}</p>
-      </div> `
-
-                  })
-
-                  // click event that will play the song in the audio player
-                  main.innerHTML = htmlstr
-                  let albums = document.querySelectorAll(".searchresult a")
-
-                  albums.forEach(function(listen) {
-                    listen.addEventListener("click", function(event) {
-                      event.preventDefault();
-                      let url = listen.getAttribute("href");
-                      document.querySelector("audio").setAttribute("src", url);
-                    })
-                  })
-})
+					albums.forEach(function(listen){
+						listen.addEventListener('click', function(event){
+							event.preventDefault();
+							let url = listen.getAttribute("href");
+							document.querySelector('audio').setAttribute('src',url);
+						})
+					})
+			})
+			}
+		)
 })
