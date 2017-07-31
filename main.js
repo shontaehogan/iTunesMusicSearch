729dc3749ea8a33389751c189f8579bca91bad84
Shontae Hogan <shontaehogan@gmail.com>
2017-07-30 20:56:47 -0400

    changes to comments

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
-/*
-  Here is a rough idea for the steps you could take:
-*/
 
-// 1. First select and store the elements you'll be working with
+
+// 1. select and store the elements
 let main = document.querySelector(".resultlist")
 let search = document.querySelector(".searchBar")
 let url = "https://itunes.apple.com/search?term="
-// 2. Create your `submit` event for getting the user's search term
+// 2. Create `submit` event for getting the search term
 search.addEventListener('keyup', function(e){
-// 3. Create your `fetch` request that is called after a submission
+// 3. Create `fetch` request called after a submission
 	let term = search.value
 	fetch(url+term)
 		.then(
@@ -18,7 +16,7 @@ search.addEventListener('keyup', function(e){
 	          console.log(response.status)
 	          return
 	        }
-// 4. Create a way to append the fetch results to your page
+// 4. append and fetch results to your page
 			response.json().then(function(data) {
 				let htmlstr = ``
 				data.results.forEach(function(data){
@@ -32,7 +30,7 @@ search.addEventListener('keyup', function(e){
      				 </div>
 					`
 				})
-// 5. Create a way to listen for a click that will play the song in the audio play
+// 5. Create a listen for a click that will play the song in the audio play
 				main.innerHTML = htmlstr
 					let albums = document.querySelectorAll('.searchresult a')
 
